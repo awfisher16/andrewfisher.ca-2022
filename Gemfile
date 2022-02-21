0ff91d73c60e83b93dd48db8bd0bae371e2fa9b4
Andrew Fisher <accounts@andrewfisher.ca>
2022-02-21 14:19:30 -0500

    removed blog and created website framework

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ source('https://rubygems.org')
 gem('jekyll', '~> 4.1')
 gem('jekyll-environment-variables')
 gem('jekyll-postcss', '~> 0.5.0')
-gem('jekyll-feed', '~> 0.12')
 
 platforms(:mingw, :x64_mingw, :mswin) do
   gem('tzinfo', '~> 1.2')
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,11 @@ module.exports = {
   content: [
     './_includes/**/*.html',
     './_layouts/**/*.html',
-    './_posts/*.{md,html}',
-    './blog/index.html',
     './index.html',
   ],
   theme: {
     extend: {
       fontFamily: {
-        'mono': '"Source Code Pro", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'
       },
     },
   },
